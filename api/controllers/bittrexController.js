eabdd78e18925a1f904bb6162bfe2837cfd2b9a1
Mike Kovetsky Asus <praskovyaseasons@gmail.com>
2017-11-15 00:49:20 +0200

    bittrex application auth / basic test route added

---
+++ api/controllers/bittrexController.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const bittrex = require('node-bittrex-api');
+bittrex.options({
+    'apikey' : 'b8171d8903844ec6b275641bd5c5d36b',
+    'apisecret' : 'SMI6R46G5YPQ5ED5',
+});
+
+exports.getSummaries = function () {
+    bittrex.getmarketsummaries( function( data, err ) {
+        if (err) {
+            return console.error(err);
+        }
+        for( var i in data.result ) {
+            bittrex.getticker( { market : data.result[i].MarketName }, function( ticker ) {
+                console.log( ticker );
+            });
+        }
+    });
+};
--- api/routes/testBotRoutes.js
+++ api/routes/testBotRoutes.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = function(app) {
-    var todoList = require('../controllers/testBotController');
+    const todoList = require('../controllers/testBotController');
+    const bittrex = require('../controllers/bittrexController');
 
-    // todoList Routes
     app.route('/tasks')
         .get(todoList.list_all_tasks)
         .post(todoList.create_a_task);
@@ -12,4 +12,7 @@ module.exports = function(app) {
         .get(todoList.read_a_task)
         .put(todoList.update_a_task)
         .delete(todoList.delete_a_task);
+
+    app.route('/bittrex/summaries')
+        .get(bittrex.getSummaries);
 };
