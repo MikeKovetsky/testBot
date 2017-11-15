4e515d2e3fe6ad4ddb6b8de7a96ce186ce485071
mkovetsky <mkovetsky@gmail.com>
2017-11-15 16:09:39 +0200

    bittrex getting neo fee

--- api/controllers/botController.js
+++ api/controllers/botController.js
@@ -3,10 +3,19 @@
 const log = require('../libs/log')(module);
 const bittrex = require('node.bittrex.api');
 
+function _getCurrency(currencies, currencyTitle) {
+    return currencies.find(currency => {
+        return currency['Currency'] === currencyTitle;
+    })
+}
 
 exports.getProfit = function (req, res) {
     bittrex.getmarkethistory({market : 'BTC-NEO'}, function (data) {
-        const lastTrade = data['result'][0];
-        res.send(lastTrade);
+        const bittrexLastTrade = data['result'][0];
+        bittrex.getcurrencies(function (data) {
+            const currencies = data['result'];
+            const bittrexNeoInfo = _getCurrency(currencies, 'NEO');
+            res.send(bittrexNeoInfo);
+        });
     });
 };
