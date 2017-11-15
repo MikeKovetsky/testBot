f08049651a7e2035c4b9e5721c74571dae29b1af
mkovetsky <mkovetsky@gmail.com>
2017-11-15 18:20:36 +0200

    kucoin signature example added

--- api/controllers/botController.js
+++ api/controllers/botController.js
@@ -10,12 +10,21 @@ function _getCurrency(currencies, currencyTitle) {
     })
 }
 
+const BITTREX_API = 'https://bittrex.com/api/v1.1/public';
+const KUCOIN_API = 'https://api.kucoin.com/v1';
+
+// {
+//     "KC-API-KEY": "5a0c6863323292218ac26b06",
+//     "KC-API-NONCE" : new Date().getTime(),
+//     "KC-API-SIGNATURE" : https://kucoinapidocs.docs.apiary.io/#introduction/authentication/signature-calculation
+// }
+
 exports.getProfit = async function (req, res) {
     const pair = ['BTC', 'NEO'];
-    const history = await client.get('https://bittrex.com/api/v1.1/public/getmarkethistory?market=' + pair[0] + '-' + pair[1]);
+    const history = await client.get(`${BITTREX_API}/getmarkethistory?market=BTC-NEO`);
     const bittrexLastTrade = history['result'][0];
-    const currencies = await client.get('https://bittrex.com/api/v1.1/public/getcurrencies');
+    const currencies = await client.get(BITTREX_API + '/getcurrencies');
     const bittrexNeoInfo = _getCurrency(currencies['result'], pair[1]);
-    const kucoinNeoInfo = await client.get("https://api.kucoin.com/v1/market/open/coin-info?coin=" + pair[1]);
+    const kucoinNeoInfo = await client.get(`${KUCOIN_API}/market/open/coin-info?coin=${pair[1]}`);
     res.send(kucoinNeoInfo);
 };
