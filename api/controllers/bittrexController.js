c03628a9799f60ce047ed0664b0340911d3e0a07
mkovetsky <mkovetsky@gmail.com>
2017-11-15 11:26:44 +0200

    bittrex node api added as a local package

--- api/controllers/bittrexController.js
+++ api/controllers/bittrexController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const bittrex = require('node-bittrex-api');
+const bittrex = require('node.bittrex.api');
 bittrex.options({
     'apikey' : 'b8171d8903844ec6b275641bd5c5d36b',
     'apisecret' : 'SMI6R46G5YPQ5ED5',
