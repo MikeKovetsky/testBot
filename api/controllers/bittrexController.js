98d91d2d1f61ec0ba768504ac07f4ecb715c0b4d
mkovetsky <mkovetsky@gmail.com>
2017-11-17 13:55:59 +0200

    bittres auth moved to botController

--- api/controllers/bittrexController.js
+++ api/controllers/bittrexController.js
@@ -2,10 +2,7 @@
 var log = require('../libs/log')(module);
 const bittrex = require('node.bittrex.api');
 
-bittrex.options({
-    'apikey': 'b8171d8903844ec6b275641bd5c5d36b',
-    'apisecret': 'SMI6R46G5YPQ5ED5',
-});
+
 
 exports.listenSocket = function (req, res) {
     bittrex.websockets.subscribe(['BTC-ETH'], function (data, client) {
--- api/controllers/botController.js
+++ api/controllers/botController.js
@@ -4,15 +4,20 @@ const log = require('../libs/log')(module);
 const bittrex = require('node.bittrex.api');
 const client = require('../libs/client');
 
+const BITTREX_API_KEY = 'b8171d8903844ec6b275641bd5c5d36b';
+const BITTREX_API = 'https://bittrex.com/api/v1.1';
+const KUCOIN_API = 'https://api.kucoin.com/v1';
+
+bittrex.options({
+    'apikey': BITTREX_API_KEY,
+    'apisecret': 'SMI6R46G5YPQ5ED5',
+});
+
 function _getCurrency(currencies, currencyTitle) {
     return currencies.find(currency => {
         return currency['Currency'] === currencyTitle;
     })
 }
-
-const BITTREX_API = 'https://bittrex.com/api/v1.1/public';
-const KUCOIN_API = 'https://api.kucoin.com/v1';
-
 // {
 //     "KC-API-KEY": "5a0c6863323292218ac26b06",
 //     "KC-API-NONCE" : new Date().getTime(),
@@ -21,10 +26,11 @@ const KUCOIN_API = 'https://api.kucoin.com/v1';
 
 exports.getProfit = async function (req, res) {
     const pair = ['BTC', 'NEO'];
-    const history = await client.get(`${BITTREX_API}/getmarkethistory?market=BTC-NEO`);
+    const history = await client.get(`${BITTREX_API}/public/getmarkethistory?market=BTC-NEO`);
     const bittrexLastTrade = history['result'][0];
-    const currencies = await client.get(BITTREX_API + '/getcurrencies');
+    const currencies = await client.get(BITTREX_API + '/public/getcurrencies');
     const bittrexNeoInfo = _getCurrency(currencies['result'], pair[1]);
     const kucoinNeoInfo = await client.get(`${KUCOIN_API}/market/open/coin-info?coin=${pair[1]}`);
-    res.send(kucoinNeoInfo);
+    const balance = await client.get(BITTREX_API + '/account/getbalances?apikey=' + BITTREX_API_KEY + '&nonce=' + new Date());
+    res.send(balance);
 };
